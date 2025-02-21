a1e19c29b1e8c43939b19dd6ba0761cfeef513b6
Richard Zille <therichkid@gmx.de>
2025-02-21 16:59:14 +0100

    Improve release script

--- build-release.mjs
+++ build-release.mjs
@@ -1,31 +1,35 @@
 import { execSync } from 'child_process';
 import ChromeExtension from 'crx';
-import { generateKeyPairSync } from 'crypto';
-import { createWriteStream, existsSync, mkdirSync, readFileSync, writeFileSync } from 'fs';
+import { access, mkdir, readFile, writeFile } from 'fs/promises';
 import { join } from 'path';
+
+const releaseDir = 'releases';
+const keyPath = join(releaseDir, 'private.pem');
 
 console.log('Building extension...');
 execSync('npm version patch && npm run build', { stdio: 'inherit' });
 
-const releaseDir = 'releases';
-const keyPath = join(releaseDir, 'private.pem');
-!existsSync(releaseDir) && mkdirSync(releaseDir);
-
-if (!existsSync(keyPath)) {
-  const { privateKey } = generateKeyPairSync('rsa', {
-    modulusLength: 2048,
-    privateKeyEncoding: { type: 'pkcs8', format: 'pem' },
-  });
-  writeFileSync(keyPath, privateKey);
+try {
+  await access(releaseDir);
+} catch {
+  await mkdir(releaseDir);
 }
 
-const crx = new ChromeExtension({
-  privateKey: readFileSync(keyPath),
-});
+const crx = new ChromeExtension();
+
+try {
+  await access(keyPath);
+  crx.privateKey = await readFile(keyPath);
+} catch {
+  await writeFile(keyPath, await crx.generateKey());
+}
 
 await crx.load('./dist');
-const packageJson = JSON.parse(readFileSync('./package.json'));
+
+const packageJson = JSON.parse(await readFile('./package.json'));
 const version = packageJson.version;
-createWriteStream(join(releaseDir, `teams-pwa-enhancements-v${version}.crx`)).end(await crx.pack());
+
+const packed = await crx.pack();
+await writeFile(join(releaseDir, `teams-pwa-enhancements-v${version}.crx`), packed);
 
 console.log(`Created release v${version}`);
