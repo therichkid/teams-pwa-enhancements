05e9a3bbe9f5c3e25d235e82200d1bf751763a9d
Richard Zille <therichkid@gmx.de>
2025-02-25 13:33:22 +0100

    Add public key to manifest

--- build-release.mjs
+++ build-release.mjs
@@ -1,12 +1,21 @@
 import { execSync } from 'child_process';
 import ChromeExtension from 'crx';
+import { createPrivateKey, createPublicKey } from 'crypto';
 import { access, mkdir, readFile, writeFile } from 'fs/promises';
 import { join } from 'path';
 
 const releaseDir = 'releases';
 const keyPath = join(releaseDir, 'private.pem');
+const manifestPath = './dist/manifest.json';
+
+const getPublicKeyForManifest = (privateKeyPem) => {
+  const privateKey = createPrivateKey(privateKeyPem);
+  const publicKey = createPublicKey(privateKey).export({ format: 'der', type: 'spki' });
+  return publicKey.toString('base64');
+};
 
 console.log('Building extension...');
+
 execSync('npm version patch && npm run build', { stdio: 'inherit' });
 
 try {
@@ -17,12 +26,19 @@ try {
 
 const crx = new ChromeExtension();
 
+let privateKeyPem;
 try {
   await access(keyPath);
-  crx.privateKey = await readFile(keyPath);
+  privateKeyPem = await readFile(keyPath, 'utf8');
+  crx.privateKey = Buffer.from(privateKeyPem);
 } catch {
-  await writeFile(keyPath, await crx.generateKey());
+  privateKeyPem = (await crx.generateKey()).toString();
+  await writeFile(keyPath, privateKeyPem);
 }
+
+const manifest = JSON.parse(await readFile(manifestPath, 'utf8'));
+manifest.key = getPublicKeyForManifest(privateKeyPem);
+await writeFile(manifestPath, JSON.stringify(manifest, null, 2));
 
 await crx.load('./dist');
 
